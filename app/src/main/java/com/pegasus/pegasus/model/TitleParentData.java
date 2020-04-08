a674654d21fe0d91666f9dc5207aeed4361d015a
ch-v-s-Naveen <vsatya951@gmail.com>
2020-04-08 15:08:07 +0530

    Working on ExpandableRecyclerview Filling

---
+++ app/src/main/java/com/pegasus/pegasus/model/TitleParentData.java
@@ -0,0 +1,15 @@
+package com.pegasus.pegasus.model;
+
+import android.os.Bundle;
+
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
+
+import java.util.List;
+
+public class TitleParentData extends ExpandableGroup {
+
+
+    public TitleParentData(String title, List items) {
+        super(title, items);
+    }
+}
